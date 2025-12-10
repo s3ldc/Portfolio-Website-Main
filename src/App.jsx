22356ddd352374b77948b6ded9fc915e59c2b4c6
Biriy <biriyasunil25@gmail.com>
2025-12-10 12:14:13 +0530

    Added particles background effect to the page

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import Experience from "./sections/Experience";
 import Testimonials from "./sections/Testimonials";
 import Contact from "./sections/Contact";
 import Footer from "./sections/Footer";
+import ParticlesBackground from "./components/ParticlesBackground";
 
 
 export default function App(){
   return(
     <div className="relative gradient text-white">
+    <ParticlesBackground/>
       <Navbar/>
       <Home/>
       <About/>
