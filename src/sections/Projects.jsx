b0aabd25ac9587e3d07cc8ccc3eecee3544831a6
Biriy <biriyasunil25@gmail.com>
2025-12-18 15:02:22 +0530

    started with the projects section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
 
-const Projects = () => {
-  return (
-    <div className='w-full h-screen'>
-      Projects
-    </div>
+export default function Projects(){
+  return(
+    <section id="projects" className="relative text-white">
+
+    </section>
   )
 }
-
-export default Projects
