4189caa90c0b65568d18401c2499a81e5b24d4fc
Biriy <biriyasunil25@gmail.com>
2025-12-19 12:03:45 +0530

    added all the images of the project

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,7 +1,11 @@
 import { useEffect, useState, useRef, useMemo } from "react"
 
-import img1 from "../assets/img1.JPG";
-import photo1 from "../assets/photo1.JPG";
+import img1 from "../assets/img1.png";
+import photo1 from "../assets/photo1.png";
+import img2 from "../assets/img2.png";
+import photo2 from "../assets/photo2.png";
+import img3 from "../assets/img3.png";
+import photo3 from "../assets/photo3.png";
 
 const useIsMobile = (query = "(max-width: 639px)") => {
   const [isMobile, setIsMobile] = useState(
@@ -26,12 +30,25 @@ export default function Projects(){
 
   const projects = useMemo(() => [
     {
-      title: "Project1",
-      link: "hhwd",
-      bgColor: "#0d4d3d",
+      title: "Tumor Detection",
+      link: "https://github.com/s3ldc/BrainTumorDetection",
+      bgColor: "#0B2F3A",
       image: isMobile ? photo1 : img1
-    }
-  ])
+    },
+    {
+      title: "Student Management",
+      link: "https://github.com/s3ldc/StudentManagementSystem",
+      bgColor: "#0F172A",
+      image: isMobile ? photo2 : img2
+    },{
+      title: "Analysis Dashboard",
+      link: "https://github.com/s3ldc/Road-Accident-Analysis",
+      bgColor: "#1E293B",
+      image: isMobile ? photo3 : img3
+    },
+  ],
+  [isMobile]
+);
 
   return(
     <section id="projects" className="relative text-white">
