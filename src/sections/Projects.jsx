27180d82416cb9d8a925a737a926a44f7443fb36
Biriy <biriyasunil25@gmail.com>
2025-12-19 11:12:54 +0530

    added one project details

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,5 +1,38 @@
+import { useEffect, useState, useRef, useMemo } from "react"
+
+import img1 from "../assets/img1.JPG";
+import photo1 from "../assets/photo1.JPG";
+
+const useIsMobile = (query = "(max-width: 639px)") => {
+  const [isMobile, setIsMobile] = useState(
+    typeof window !== 'undefined' && window.matchMedia(query).matches
+  )
+
+  useEffect(() => {
+    if(typeof window === "undefined") return;
+    const mql = window.matchMedia(query);
+    const handler = (e) => setIsMobile(e.matches);
+
+    mql.addEventListener("change", handler);
+    setIsMobile(mql.matches);
+    return () => mql.removeEventListener("change", handler);
+  }, [query])
+  return isMobile;
+}
 
 export default function Projects(){
+  const isMobile = useIsMobile();
+  const sceneRef = useRef(null);
+
+  const projects = useMemo(() => [
+    {
+      title: "Project1",
+      link: "hhwd",
+      bgColor: "#0d4d3d",
+      image: isMobile ? photo1 : img1
+    }
+  ])
+
   return(
     <section id="projects" className="relative text-white">
 
