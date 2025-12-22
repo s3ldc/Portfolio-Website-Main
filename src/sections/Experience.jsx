265a0af490f9cf0ed72a497b8a06058ef280b0b3
Biriy <biriyasunil25@gmail.com>
2025-12-22 16:49:05 +0530

    added job details in the created array

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -9,16 +9,16 @@ const experiences = [
   {
     role: "Web Developer Intern",
     company: "ExclCloud",
-    duration: "Dec 2024 – Feb 2025",
+    duration: "2024 – 2025",
     description:
       "Collaborated with cross-functional teams to build high-performance SvelteKit applications for 1,200+ users, delivering automation features that reduced latency by 22%, cut meeting coordination time by 30%, and increased employee engagement by 40%.",
   },
   {
-    role: "Web Developer Intern",
-    company: "ExclCloud",
-    duration: "Dec 2024 – Feb 2025",
+    role: "Freelance WordPress Developer",
+    company: "IndoGerman Education Consultancy",
+    duration: "2025",
     description:
-      "Collaborated with cross-functional teams to build high-performance SvelteKit applications for 1,200+ users, delivering automation features that reduced latency by 22%, cut meeting coordination time by 30%, and increased employee engagement by 40%.",
+      "Developed an automated CSV import and PDF report generation system with an admin dashboard, reducing manual workload by 95% across 500+ records, achieving 100% data accuracy, and improving data retrieval speed by 70%.",
   },
 ];
 
