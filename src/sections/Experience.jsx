2187e17d3e231c760d10d81983a548aa11549ed0
Biriy <biriyasunil25@gmail.com>
2025-12-22 16:46:37 +0530

    started working on the my experience section

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,9 +1,29 @@
+const experiences = [
+  {
+    role: "Web Developer Intern",
+    company: "ExclCloud",
+    duration: "Dec 2024 – Feb 2025",
+    description:
+      "Collaborated with cross-functional teams to build high-performance SvelteKit applications for 1,200+ users, delivering automation features that reduced latency by 22%, cut meeting coordination time by 30%, and increased employee engagement by 40%.",
+  },
+  {
+    role: "Web Developer Intern",
+    company: "ExclCloud",
+    duration: "Dec 2024 – Feb 2025",
+    description:
+      "Collaborated with cross-functional teams to build high-performance SvelteKit applications for 1,200+ users, delivering automation features that reduced latency by 22%, cut meeting coordination time by 30%, and increased employee engagement by 40%.",
+  },
+  {
+    role: "Web Developer Intern",
+    company: "ExclCloud",
+    duration: "Dec 2024 – Feb 2025",
+    description:
+      "Collaborated with cross-functional teams to build high-performance SvelteKit applications for 1,200+ users, delivering automation features that reduced latency by 22%, cut meeting coordination time by 30%, and increased employee engagement by 40%.",
+  },
+];
 
-
-export default function Experience(){
-  return(
-    <section id="experience" className="relative bg-black text-white">
-
-    </section>
-  )
+export default function Experience() {
+  return (
+    <section id="experience" className="relative bg-black text-white"></section>
+  );
 }
