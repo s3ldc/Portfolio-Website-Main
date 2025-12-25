526d15167f42032eea9f872d5ea283bcceb9b777
Biriy <biriyasunil25@gmail.com>
2025-12-25 20:41:21 +0530

    add email service credentials via environment variables

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,6 +1,10 @@
 import ParticlesBackground from "../components/ParticlesBackground";
 
 
+const SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
+const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID;
+const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
+
 export default function Contact(){
   return(
     <section id="contact" className="w-full min-h-screen relative bg-black overflow-hidden text-white py-20 px-6 md:px-20 flex flex-col md:flex-row items-center gap-10">
