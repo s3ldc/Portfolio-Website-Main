82c5626e6b87d07f905cc626fa8988d2a1d3b5b6
Biriy <biriyasunil25@gmail.com>
2025-12-25 20:53:40 +0530

    add numeric-only validation for budget field and clear field errors on change

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import ParticlesBackground from "../components/ParticlesBackground";
 
 
@@ -6,6 +7,26 @@ const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID;
 const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
 
 export default function Contact(){
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    service: "",
+    budget: "",
+    idea: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const [status, setStatus] = useState("");
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    if(name === "budget" && value && !/^\d+$/.test(value)) return;
+    setFormData((p) => ({...p, [name] : value}));
+    if(errors[name]) setErrors((p) => ({...p, [name] : ""}))
+  }
+
   return(
     <section id="contact" className="w-full min-h-screen relative bg-black overflow-hidden text-white py-20 px-6 md:px-20 flex flex-col md:flex-row items-center gap-10">
       <ParticlesBackground/>
